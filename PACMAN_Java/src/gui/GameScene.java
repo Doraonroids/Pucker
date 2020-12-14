6f939a0bb7d42479dedf296728680f786defe528
Doraonroids <59835131+Doraonroids@users.noreply.github.com>
2020-12-13 23:20:47 -0500

    idk

--- PACMAN_Java/src/gui/GameScene.java
+++ PACMAN_Java/src/gui/GameScene.java
@@ -1,14 +1,32 @@
 package gui;
 
 
+
+
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
+
 public class GameScene extends Scene {
+    private static GridPane root = new GridPane();
 
 
-    public GameScene(Parent root) {
-        super(root);
+    public GameScene() {
+        super(root,600,800);
     }
+
+   public void addStuff(){
+       root.add(new Line(0,0,2,2),0,0);
+
+   }
 }
